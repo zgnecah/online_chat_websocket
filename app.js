9ef28de02a7df2b2690f89e79757d19625602c72
zgnecah <zgnecah@163.com>
2016-01-28 10:30:26 +0800

    complete socket.io

--- app.js
+++ app.js
@@ -5,6 +5,55 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+io.on('connection', function(socket) {
+    socket.emit('open');
+    var client = {
+        socket: socket,
+        name: false,
+        color: getColor()
+    }
+
+    socket.on('message', function(msg) {
+        var obj = {
+            time: getTime(),
+            color: client.color
+        };
+        if (!client.name) {
+            client.name = msg;
+            obj['text'] = client.name;
+            obj['auther'] = 'System';
+            obj['type'] = 'welcome';
+            console.log(client.name + ' login');
+
+            socket.emit('system', obj);
+            socket.broadcast.emit('system', obj);
+        } else {
+            obj['text'] = msg;
+            obj['auther'] = client.name;
+            obj['type'] = 'message';
+            console.log(client.anme + ' sya: ' + msg);
+            socket.emit('message', obj);
+            socket.broadcast.emit('message', obj);
+        }
+    });
+
+    socket.on('disconnect', function() {
+        var obj = {
+          time: getTime(),
+          color: client.color,
+          auther: 'System',
+          text: client.name,
+          type: 'disconnect'
+        };
+
+        socket.broadcast.emit('system', obj);
+        console.log(client.name + 'Disconnect.');
+    });
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,7 +67,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,9 +78,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +88,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
